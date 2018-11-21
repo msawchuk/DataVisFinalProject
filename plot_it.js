9f991103e62500dcca9718636ed6ef41364955f7
Cole Sawyer <cole.g.sawyer@vanderbilt.edu>
2018-11-21 12:42:52 -0600

    some changes

--- plot_it.js
+++ plot_it.js
@@ -1,15 +1,13 @@
 //given a ticker object, calculates probability of
 var start_date;
 var end_date;
-function probability_density(ticker, val){
-    var num = Math.round(val*ticker.children.length);
-    return ticker.children[i].avg
-}
+
 function expected_value(ticker){
     var sum = 0;
     for(var i = 0; i<ticker.children.length; i++){
         sum += ticker.children[i].avg;
     }
+    ticker.ev = sum/ticker.children.length;
     return sum/ticker.children.length;
 }
 function std_dev(ticker){
@@ -18,14 +16,32 @@ function std_dev(ticker){
     for(var i = 0; i<ticker.children.length; i++){
         deviation += Math.power(ev-ticker.children[i].avg, 2);
     }
+    ticker.deviation = deviation
+    ticker.std_deviation = Math.sqrt(deviation);
     return Math.sqrt(deviation);
 }
-function get_avg_deviation(array){
+//given an array of tickers, calculates the average standard deviation
+function get_avg_std_deviation(array){
+    var deviation = 0;
     for(var i = 0; i < array.length; i++){
-        
+        deviation += arrya[i].deviation;
+    }
+    return Math.sqrt(deviation);
+}
+function get_std_dev_of_std_dev(array){
+    var avg = get_avg_std_deviation(array)
+    var dev = 0;
+    for(var i = 0; i< array.length; i++){
+        dev += Math.power(avg - array[i].deviation, 2);
+    }
+    return Math.sqrt(dev)
+}
+function calculate_std_dev_dev(array){
+    var std_dev = get_std_dev_of_std_dev()
+    for(var i = 0; i<array.length; i++){
+        array[i].std_dev_std_dev = array[i].avg - std_dev;
     }
 }
-
 function plot_it() {
     start_date = new Date(2017, 0, 1);
     end_date = new Date(2018, 0, 1);
@@ -34,11 +50,12 @@ function plot_it() {
     preprocess_tree(price_data, '', 0, start_date, end_date);
     //need to change when we add more stuff
     for(var i = 0; i <price_data.children.length; i++){
-        find_valid_data(price_data.children[i])
+        find_valid_data(price_data.children[i]);
     }
+    console.log(price_data);
     aggregate(price_data);
 
-    console.log(price_data);
+
 }
 
 function find_valid_data(node){
@@ -60,6 +77,7 @@ function preprocess_tree(node, concat_names, depth) {
     if('children' in node)  {
         node.is_leaf = false;
         node.value = 0;
+        node.valid_data = node.children;
         for(var c = 0; c < node.children.length; c++)  {
             node.children[c].parent = node;
             preprocess_tree(node.children[c], node.full_name, node.depth);
@@ -67,7 +85,7 @@ function preprocess_tree(node, concat_names, depth) {
     }
     else  {
         node.is_leaf = true;
-        node.value = +node.value;
+        node.value = +node.Close;
         node.children = [];
         node.Date = new Date(Date.parse(node.Date));
         if(node.Date.getTime() >= start_date.getTime() && node.Date.getTime() <= end_date.getTime()){
@@ -80,13 +98,14 @@ function preprocess_tree(node, concat_names, depth) {
 }
 
 function aggregate(node) {
-    if(node.children.length > 0) {
+    console.log(node)
+    if(node.valid_data.length > 0) {
         for(var a = 0; a < node.children.length; a++) {
             aggregate(node.children[a]);
         }
+        for(var b = 0; b < node.valid_data.length; b++) {
+            node.value += node.valid_data[b].value;
+        }
+        node.value /= node.valid_data.length
     }
-    for(var b = 0; b < node.children.length; b++) {
-        node.value += node.children[b].value;
-    }
-    node.value /= node.children
 }
